31140dfbf2d58515dab1a94fc88567f00c267c99
Alina-Zadorojnaya <alinasmartmebel@gmail.com>
2021-10-09 22:24:45 +0300

    mobil finish

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(function(){
 
+    $('.mobil-btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
+  /*  $('.response__partners').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
+    */
 });
+
